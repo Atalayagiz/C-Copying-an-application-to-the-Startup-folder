99798b71a40265aedce826752b49065283cb0e7a
Atalayagiz <78467521+Atalayagiz@users.noreply.github.com>
2022-05-03 20:37:43 +0300

    Add files via upload

---
+++ Application_Copy_Itself_To_Startup/Application_Copy_Itself_To_Startup/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Application_Copy_Itself_To_Startup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string thisFile = System.AppDomain.CurrentDomain.FriendlyName;
+
+            string Path = AppDomain.CurrentDomain.BaseDirectory + "\\" + thisFile;
+
+            string Filepath = Environment.GetFolderPath(Environment.SpecialFolder.Startup) + "\\" + thisFile;
+
+
+            try
+
+            {
+                //COPY THIS PROGRAM TO STARTUP
+                if (!File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.Startup) + "\\" + thisFile))
+                {
+                    System.IO.File.Copy(Application.ExecutablePath, Filepath);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(e.ToString());
+            }
+
+        }
+    }
+}
